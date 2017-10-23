895bb1e0f771ec84b83dec42e737cc4197ff5bb1
Abdullah Hashmi <abdullah.hashmi21@gmail.com>
2017-10-22 20:30:10 -0400

    First Commit

---
+++ DailyPlanet/DailyPlanetPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using Scanner;
+using ZXing;
+
+namespace DailyPlanet
+{
+    public partial class DailyPlanetPage : ContentPage
+    {
+        public DailyPlanetPage()
+        {
+            InitializeComponent();
+        }
+
+        async void onScanClicked(object sender, EventArgs e)
+        {
+            var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+
+            var result = await scanner.Scan();
+
+            if (result != null)
+                await DisplayAlert("Scanner", result.Text, "cancel");
+
+        }
+        async void onTableBtnClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("This is where database will display", "DATABASE", "cancel");
+        }
+    }
+}
---
+++ DailyPlanet/ScannerPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Scanner
+{	
+	public partial class ScannerPage : ContentPage
+	{	
+		public ScannerPage ()
+		{
+			InitializeComponent ();
+		}
+		async void onScanClicked(object sender, EventArgs e)
+		{
+			var data = await DependencyService.Get<IScanner> ().Scan ();
+
+			DisplayAlert ("Scanner", data, "cancel");
+		}
+	}
+}
+
